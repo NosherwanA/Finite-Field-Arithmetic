8c5a2e2310dbb39d6bdb294a53a2aa94a8159261
NosherwanA <nosherwan.a2@gmail.com>
2017-11-26 12:48:18 -0500

    Added more test cases for Modular Exponentiator

--- tb/tb_Modular_Exponentiator.vhd
+++ tb/tb_Modular_Exponentiator.vhd
@@ -74,6 +74,61 @@ architecture test of tb_Modular_Exponentiator is
             --result: 00001101  (13)10
             wait for DELTA_TIME;
 
+
+            in_reset <= '0';
+            in_base <= "11101010"; -- (234)10
+            in_exponent <= "11000101"; -- (197)10
+            in_modulus <= "11011000"; -- (216)10
+            wait for 15 ns;
+            in_reset <= '1';
+            in_start <= '1';
+            wait for 20 ns;
+            in_start <= '0';
+            wait until (out_done = '1');
+            --result: 00000000  (0)10
+            wait for DELTA_TIME;
+
+
+            in_reset <= '0';
+            in_base <= "00000011"; -- (3)10
+            in_exponent <= "11101000"; -- (232)10
+            in_modulus <= "00000100"; -- (4)10
+            wait for 15 ns;
+            in_reset <= '1';
+            in_start <= '1';
+            wait for 20 ns;
+            in_start <= '0';
+            wait until (out_done = '1');
+            --result: 00000001  (1)10
+            wait for DELTA_TIME;
+
+
+            in_reset <= '0';
+            in_base <= "10010011"; -- (147)10
+            in_exponent <= "11000110"; -- (198)10
+            in_modulus <= "10110111"; -- (183)10
+            wait for 15 ns;
+            in_reset <= '1';
+            in_start <= '1';
+            wait for 20 ns;
+            in_start <= '0';
+            wait until (out_done = '1');
+            --result: 00001001  (9)10
+            wait for DELTA_TIME;
+
+
+            in_reset <= '0';
+            in_base <= "11110110"; -- (246)10
+            in_exponent <= "00111001"; -- (57)10
+            in_modulus <= "01101000"; -- (104)10
+            wait for 15 ns;
+            in_reset <= '1';
+            in_start <= '1';
+            wait for 20 ns;
+            in_start <= '0';
+            wait until (out_done = '1');
+            --result: 01000000  (64)10
+            wait for DELTA_TIME;
             
 
          end process simulation;   
